506ba009e13ef1ac4d1095c3e987154b9249066c
Alexander <aleasims@gmail.com>
2019-10-08 16:28:50 +0300

    Removed attrnodes, now all nodes have attrs

--- parsefw/structure/tree.py
+++ parsefw/structure/tree.py
@@ -7,14 +7,29 @@ TNode = TypeVar('Node')
 
 
 class Node:
-    """Tree node."""
+    """Attribute tree node.
 
-    def __init__(self, childs: Optional[List[TNode]] = None,
+    Attributes:
+        parent (Node) - parent node
+        childs (List[Node]) - list of child nodes
+        label (str) - if not set, will be generated
+    """
+
+    def __init__(self,
                  parent: Optional[TNode] = None,
-                 label: Optional[str] = None):
+                 childs: Optional[List[TNode]] = None,
+                 label: Optional[str] = None,
+                 **kwargs):
         self.childs = childs if childs is not None else []
         self.parent = parent
-        self.label = label if label is not None else 'node_{}'.format(id(self))
+        self.label = label if label is not None else '{}_{}'.format(
+            self.classname(), id(self))
+        self.attrs = list(kwargs.keys()) + ['label']
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def classname(self):
+        return self.__class__.__name__
 
     def add_child(self, node: TNode, position: Optional[int] = None):
         position = len(self.childs) if position is None else position
@@ -33,40 +48,22 @@ class Node:
         yield from dfs(self)
 
     def __str__(self):
-        return '<{type} {label}>'.format(
-            type=self.__class__.__name__,
+        return '{type} {label}'.format(
+            type=self.classname(),
             label=self.label)
 
     def __repr__(self):
-        return '<{type} {label}>'.format(
-            type=self.__class__.__name__,
-            label=self.label)
-
-
-class AttrNode(Node):
-    """Attribute tree node."""
-
-    def __init__(self, childs=None, parent=None, label=None, **kwargs):
-        super().__init__(childs, parent, label)
-        self.attrs = kwargs
-
-    def get_attr(self, name):
-        return self.attrs[name]
-
-    def set_attr(self, name, value):
-        self.attrs[name] = value
-
-    def __getitem__(self, key):
-        return self.get_attr(key)
-
-    def __setitem__(self, key, value):
-        self.set_attr(key, value)
-
-    def __repr__(self):
-        return '<{type} {label} [{attrs}]>'.format(
+        return '<{type} {label} {attrs}>'.format(
             type=self.__class__.__name__,
             label=self.label,
-            attrs=', '.join([str(key) for key in self.attrs.keys()]))
+            attrs=self.attrs)
+
+
+class Tree(Node):
+    """Represents root node."""
+
+    def __init__(self, childs=None, label=None, **kwargs):
+        super().__init__(None, childs, label, **kwargs)
 
 
 def dfs(node,
